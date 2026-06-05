e21f58a55d195b97a9295a8fcbc552d87e8f8da5
ayahzyoud <ayahmohammad341@gmail.com>
2026-06-04 23:43:24 -0700

    update

---
+++ visualizer.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Visualizer:
+
+    def bar_chart(self, students):
+        pass
+
+    def histogram(self, students):
+        pass
+
+    def pie_chart(self, stats):
+        pass
